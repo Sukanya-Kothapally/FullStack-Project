1d4af8b6691fd67b9124573d7c561dd664d01377
nishnareddy1 <aletinishna0723@gmail.com>
2020-05-28 15:56:00 -0700

    adding credential details for apimedic

--- credentials.py
+++ credentials.py
@@ -2,3 +2,5 @@ priaid_auth_url = "https://authservice.priaid.ch/login"  # https://sandbox-auths
 priaid_health_url = "https://healthservice.priaid.ch"  # https://sandbox-healthservice.priaid.ch
 username = "Qo35P_GMAIL_COM_AUT"
 password = "y4J3YcMe67Bxi9PLb"
+language = "en-gb"
+printRawOutput = False
