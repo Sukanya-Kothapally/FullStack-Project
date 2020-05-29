8acefd367c97033b8f33f1a483944a0b6126033f
sowmya-indukuri <indukuri@pdx.edu>
2020-05-28 22:18:12 -0700

    locationform.htmladded

--- app.py
+++ app.py
@@ -14,10 +14,21 @@ def index():
 
 #button2 home page
 @app.route('/live+/location/', methods=['POST','GET'])
-def location():
+def enterLocation():
         return render_template("locationform.html")
 
+#button2 resultspage
+@app.route("/live+/hospitalsresult/",methods=['POST','GET'])
+def locationHospital():
+        googleMaps = googlemaps.Client(key='AIzaSyAQO23hWoMorbHodnChbFZ42g-BiGEcSqM') #connecting to google
+        location = request.form["location"]  # getting address entered by user in form
+        geocoding_result = googleMaps.geocode(location)
 
+        locationLatLng=[(float(i['geometry']['location']['lat']), float(i['geometry']['location']['lng'])) for i in geocoding_result] #extracting latitude and longitude using the address enetered by user
+        result=googleMaps.places(query="hospital", location=locationLatLng[0], radius = '100')['results'] #getting the name of hospitals near the address entered by user
+
+        hospitalResult=[[str(r['name']),str(r['formatted_address']),str(r['user_ratings_total']), str(r['rating'])] for r in result]
+        return render_template("hospitalsList.html", res=hospitalResult)
 
 
 if __name__ == '__main__':
