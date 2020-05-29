fc4ce8679b7ecb105e0304fcb4eebce321e481bd
nishnareddy1 <aletinishna0723@gmail.com>
2020-05-29 01:35:16 -0700

    adding rout to app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import json
 from flask import Flask, render_template, request
-
+import get_symptoms as get_sym
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/medical_conditions/')
+def two():
+    symptoms = get_sym.fetch_symptoms()
+    return render_template("medical_conditions.html", symptoms=symptoms)
 
 if __name__ == '__main__':
     app.run(debug=True)
