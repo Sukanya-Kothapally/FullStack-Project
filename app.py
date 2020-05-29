b083c1c59d1437b58cf4923a5536132d33271613
sowmya-indukuri <indukuri@pdx.edu>
2020-05-28 19:31:23 -0700

    routingforsecondbutton

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import json
 from flask import Flask, render_template, request
+from flask import render_template,make_response
+from flask import Flask, request, redirect, url_for
+
+import googlemaps
 
 app = Flask(__name__)
 
+# home page
 @app.route('/')
 def index():
     return render_template("index.html")
+
+#button2 home page
+@app.route('/live+/location/', methods=['POST','GET'])
+def location():
+        return render_template("locationform.html")
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
