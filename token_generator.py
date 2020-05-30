642241b3dba06db04b34e2581e32af6ec52d176b
nishnareddy1 <aletinishna0723@gmail.com>
2020-05-30 14:06:33 -0700

    changing file name

--- token_generator.py
+++ token_generator.py
@@ -1,4 +1,4 @@
-import crendentials
+import config
 import requests
 import base64
 import hmac
@@ -6,9 +6,9 @@ import json
 
 # Generating token for APIMEDIC.
 def tokenGen():
-    username = crendentials.username
-    password = crendentials.password
-    authUrl = crendentials.priaid_auth_url
+    username = config.username
+    password = config.password
+    authUrl = config.priaid_auth_url
 
     HashString = hmac.new(bytes(password, encoding='utf-8'), authUrl.encode('utf-8')).digest()
     computedHashString = base64.b64encode(HashString).decode()
