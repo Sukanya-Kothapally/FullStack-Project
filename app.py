81ed82f34dd034391f58a936ae48056304c207bf
Sukanya-Kothapally <58608694+Sukanya-Kothapally@users.noreply.github.com>
2020-05-30 17:33:29 -0700

    recreation files

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import recreation as rec
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/live+/recreation/', methods=['POST','GET'])
+def location():
+        return render_template("recreationform.html")
+
+@app.route("/live+/recreationdata/",methods=['POST','GET'])
+def map_doc():
+        recDict=rec.fetch()
+        locationfromhtml=rec.locationfunction()
+        if(recDict=="No Location given, Please give a place!!!"):
+            return render_template("recreationform.html", recDict=recDict)
+        else:
+            Dictstr=json.dumps(recDict)
+            resultsdata=json.loads(Dictstr)
+            return render_template("recreation.html", resultsdata=resultsdata,locationfromhtml=locationfromhtml)
 
 if __name__ == '__main__':
     app.run(debug=True)
