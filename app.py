9a0ac4dcafbb0bd1dac0b9e5109b7769a34bc596
sowmya-indukuri <indukuri@pdx.edu>
2020-05-30 22:32:59 -0700

    changes in app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import json
 from flask import Flask, render_template, request
-from flask import render_template,make_response
+from flask import render_template,make_response, jsonify
 from flask import Flask, request, redirect, url_for
+import hospitals as hs
 
 import googlemaps
 
@@ -20,15 +21,19 @@ def enterLocation():
 #button2 resultspage
 @app.route("/live+/hospitalsresult/",methods=['POST','GET'])
 def locationHospital():
-        googleMaps = googlemaps.Client(key='AIzaSyAQO23hWoMorbHodnChbFZ42g-BiGEcSqM') #connecting to google
-        location = request.form["location"]  # getting address entered by user in form
-        geocoding_result = googleMaps.geocode(location)
+    hosDict = hs.fetch()
+    locationfromhtml = hs.locationfunction()
+    if (hosDict == "No Location given, Please give a place!!!"):
+        return render_template("locationform.html", res=hosDict)
+    else:
+        Dictstr = json.dumps(hosDict)
+        resultsdata = json.loads(Dictstr)
+        return render_template("hospitalsList.html", res=resultsdata, locationfromhtml=locationfromhtml)
 
-        locationLatLng=[(float(i['geometry']['location']['lat']), float(i['geometry']['location']['lng'])) for i in geocoding_result] #extracting latitude and longitude using the address enetered by user
-        result=googleMaps.places(query="hospital", location=locationLatLng[0], radius = '100')['results'] #getting the name of hospitals near the address entered by user
 
-        hospitalResult=[[str(r['name']),str(r['formatted_address']),str(r['user_ratings_total']), str(r['rating'])] for r in result]
-        return render_template("hospitalsList.html", res=hospitalResult)
+
+
+
 
 
 if __name__ == '__main__':
