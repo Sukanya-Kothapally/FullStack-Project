fcc1b8f78a7277cf312228bbbc90f779f84349af
nishnareddy1 <aletinishna0723@gmail.com>
2020-06-03 01:31:42 -0700

    changing the api data type

--- symptoms_result.py
+++ symptoms_result.py
@@ -5,14 +5,12 @@ import urllib.request
 
 authKey = token.token_gen()
 
-
 def api_symptom_result(symptoms_ids, gender, year_of_birth):
-    urlMedicalCondition = config.priaid_health_url + "/diagnosis?symptoms=[" + symptoms_ids + "]&gender=" + gender + "&year_of_birth=" + year_of_birth + "&token=" + authKey + "&format=json&language=en-gb"
-    resultsMC = urllib.request.urlopen(urlMedicalCondition).read()  # returns a byte string
-    api_results = ast.literal_eval(resultsMC.decode("utf-8"))  # converting the byte string to string and then parsing it into a list
     api_results_dict = {}
-    for issue in api_results:
-        accuracy=issue['Issue']['Accuracy']
-        api_results_dict[accuracy] = issue['Issue']['Name']
-
+    get_symptoms_result = config.priaid_health_url + "/diagnosis?symptoms=[" + symptoms_ids + "]&gender=" + gender + "&year_of_birth=" + year_of_birth + "&token=" + authKey + "&format=json&language=en-gb"
+    results = urllib.request.urlopen(get_symptoms_result).read()
+    api_results = ast.literal_eval(results.decode("utf-8"))
+    for result in api_results:
+        accuracy = result['Issue']['Accuracy']
+        api_results_dict[accuracy] = result['Issue']['Name']
     return api_results_dict
