b43c16cabdeb1170a2af69f52ed30db29576c06e
nishnareddy1 <aletinishna0723@gmail.com>
2020-06-03 19:18:51 -0700

    changing api credentials

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 priaid_auth_url = "https://authservice.priaid.ch/login"  # https://sandbox-authservice.priaid.ch
 priaid_health_url = "https://healthservice.priaid.ch"  # https://sandbox-healthservice.priaid.ch
-username = "Qo35P_GMAIL_COM_AUT"
-password = "y4J3YcMe67Bxi9PLb"
+username = "z3P4C_GMAIL_COM_AUT"
+password = "j2J4WyBx95Gaw7M8N"
 language = "en-gb"
 printRawOutput = False
